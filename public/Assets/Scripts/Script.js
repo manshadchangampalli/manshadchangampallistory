bea3f9c4dd1da735ca88157fdfb0de08eb50a52d
manshad <manshadchangampalli010@gmail.com>
2021-10-30 11:06:50 +0530

    UPDATE : #the auto scrolling is prevented when the input focus

--- public/Assets/Scripts/Script.js
+++ public/Assets/Scripts/Script.js
@@ -382,7 +382,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .to('.ui-ux h2',3,{
         display:'block'
     },'-=6')
-    .to('.ui-ux h2',10,{
+    .to('.ui-ux h2',100,{
         display:'block'
     })
     .to('.ui-ux h2',5,{
